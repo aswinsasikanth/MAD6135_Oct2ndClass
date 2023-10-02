2ed085bed219591f94e8887fa1bf2081ea54d00b
Aswin_Sasikanth <sasikanthh@gmail.com>
2023-10-02 13:21:55 -0400

    inclass activity to create 4 functions / objects

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 // var a = iamafucntion('cat')
 // console.log(a);
 
-
+/*
 //Simple function to create Object
 function iamafucntion(z) {
     function sum(m,n){
@@ -16,3 +16,56 @@ function iamafucntion(z) {
 }
 var a = iamafucntion('cat')
 console.log(a);
+*/
+
+// Prgram to create 4 functions and each serving as an object
+
+//function for object tiger
+function Tiger(name){
+    this.name = name;
+    this.sound = "Roar";
+    this.makesound = function(){
+        console.log(this.name + " says " + this.sound)
+    };
+}
+
+//function for object Lion
+function Lion(name){
+    this.name = name;
+    this.sound = "Roar";
+    this.makesound = function(){
+        console.log(this.name + " says " + this.sound)
+    };
+}
+
+//function for object Monkey
+function Monkey(name){
+    this.name = name;
+    this.sound = "Chatter";
+    this.makesound = function(){
+        console.log(this.name + " says " + this.sound)
+    };
+}
+
+//function for object Snake
+function Snake(name){
+    this.name = name;
+    this.sound = "Hiss";
+    this.makesound = function(){
+        console.log(this.name + " says " + this.sound)
+    };
+}
+
+//Creating objects for each animal
+var tiger = new Tiger("Tiger");
+var lion = new Lion("Lion");
+var monkey = new Monkey("Monkey");
+var snake = new Snake("Snake");
+
+var jungleAnimals = [tiger, lion, monkey, snake];
+
+jungleAnimals.forEach(function(animal){
+    animal.makesound();
+})
+
+
