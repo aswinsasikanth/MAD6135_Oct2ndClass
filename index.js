e8de877015276ff7b516db8b481383b3ee81d7b1
Aswin_Sasikanth <sasikanthh@gmail.com>
2023-10-02 12:58:55 -0400

    simple funtion to create Objs

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 // Make simple function
+// function iamafucntion(z) {
+//     let a = z
+//     return a
+// }
+// var a = iamafucntion('cat')
+// console.log(a);
+
+
+//Simple function to create Object
 function iamafucntion(z) {
+    function sum(m,n){
+    return(m+n)}
     let a = z
-    return a
+    return sum(2,2)
 }
 var a = iamafucntion('cat')
 console.log(a);
